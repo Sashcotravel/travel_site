ad2da722ee19222d28886ff42282d2a340407fde
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-08-31 21:26:57 +0300

    update cart countries

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Destination1 from "../assets/Destination1.png";
-import Destination2 from "../assets/Destination2.png";
-import Destination3 from "../assets/Destination3.png";
-import Destination4 from "../assets/Destination4.png";
-import Destination5 from "../assets/Destination5.png";
-import Destination6 from "../assets/Destination6.png";
+import Destination1 from "../assets/Egipt.jpeg";
+import Destination2 from "../assets/Turkey.jpg";
+import Destination3 from "../assets/Montenegro.jpeg";
+import Destination4 from "../assets/Greece.jpg";
+import Destination5 from "../assets/Bulgaria.jpg";
+import Destination6 from "../assets/Cyprus.jpeg";
 import Page1 from "../Travel Page/page1";
 import Page4 from "../Travel Page/page4";
 import Page3 from "../Travel Page/page3";
@@ -15,43 +15,33 @@ export default function Recommend() {
   const data = [
     {
       image: Destination1,
-      title: "Singapore",
-      subTitle: "Singapore, officialy thr Republic of Singapore, is a",
-      cost: "38,800",
-      duration: "Approx 2 night trip",
+      title: "Египет",
+      subTitle: "Якщо ви плануєте сімейний відпочинок з дітьми, то напевно вже знаєте, що одним з найпопулярніших напрямків для такої поїздки є Єгипет. І це цілком виправдано! Комфортний переліт, відмінна інфраструктура, найчистіші пляжі з блакитними лагунами і кораловими рифами. Вибір готелів для сімейного відпочинку в Єгипті справді великий. Тут можна підібрати варіант на будь-який смак і гаманець. При цьому майже в кожному готелі для маленьких туристів обладнаний дитячий клуб з цікавою розважальною програмою, невеликий басейн і навіть водні гірки, а вечорами проходять захоплюючі вистави та музичні шоу для всієї родини.",
     },
     {
       image: Destination2,
-      title: "Thailand",
-      subTitle: "Thailand is a Southeast Asia country. It's known for",
-      cost: "54,200",
-      duration: "Approx 2 night trip",
+      title: "Туреччина",
+      subTitle: "І звичайно, в Туреччині дітей чекає величезна кількість розваг. Практично кожен день з гавань Кемера в невеликі одноденні круїзи, уздовж узбережжя Середземного моря, відправляються дерев'яні парусні яхти - гюлети. Парк «Місячне сяйво» - найпопулярніше місце відпочинку в Кемері. Парк Moonlight, де розташований єдиний пісочний пляж в місті, атракціони, дельфінарій і ресторани, а ще безліч дельфінаріїв і аквапарків!",
     },
     {
       image: Destination3,
-      title: "Paris",
-      subTitle: "Paris, France's capital, is a major European city and a",
-      cost: "45,500",
-      duration: "Approx 2 night trip",
+      title: "Чорногорія",
+      subTitle: "Крім цього, діти тут зможуть добре оздоровитися, адже одна з граней цієї чудової країни - лікування та оздоровлення! З давніх-давен пісок і вода деяких пляжів Чорногорії відомі своїми цілющими властивостями. Так, відомий Ульцинський Великий пляж вкритий найдрібнішим піском. Він багатий мінералами, відпочинок на цьому пляжі в нагоді тим, хто хворий на ревматизм, артрит або м'язовими захворюваннями. Та й веселих забав тут дуже багато! Наприклад, парк пригод Ловчен - місце для веселих і активних. У цьому унікальному місці можна випробувати себе, долаючи перешкоди на мотузкових атракціонах на вершинах дерев. Парк «Ловчен», або «Ігри серед дерев» - єдиний проект такого роду на Балканах.",
     },
     {
       image: Destination4,
-      title: "New Zealand",
-      subTitle: "New Zealand is an island country in the",
-      cost: "24,100",
-      duration: "Approx 1 night trip",
+      title: "Греція",
+      subTitle: "Греція - дуже затишна країна для сімейного відпочинку. Тут є все, щоб відчувати себе як вдома, де є море, смачна їжа, різні сири і фантастичні вина. Відпочинок з дітьми тут буде радісним і легким, адже крім оздоровлення для дітей тут є дуже багато місць, щоб розважатися, як з батьками так і без. Радимо відвідати акваріум «Водний світ» - перший акваріум на Криті був створений як реабілітаційний центр для потерпілих від восьминогів, черепашок, крокодилів. Планувати поїздку в аквапарк Water Park треба заздалегідь і розраховувати на цілий день. За менший час тут просто не впоратися.",
     },
     {
       image: Destination5,
-      title: "Bora Bora",
-      subTitle: "Bora Bora is a small South Pacific island northwest of",
-      cost: "95,400",
-      duration: "Approx 2 night 2 day trip",
+      title: "Болгарія",
+      subTitle: "У готелях з дитячим клубом і анімацією з вашим малюком будуть займатися професійні аніматори. Цілий день з дітьми малюють, читають, ліплять з пластиліну, грають в цікаві рухливі ігри, які розвивають фантазію. Хорошим прикладом сервісу для дітей слугують дитячі клуби «Solvex», організовані нашими партнерами. Щоб переконатися в перевагах клубу, експерти туроператора Join UP! рекомендують сім'ям з дітьми вибирати, наприклад, такі готелі: Park Hotel Odessos 4 * Золоті піски, Elitsa 3 * + Албена, Yunona 3 * Сонячний берег, Atrium Beach / Andalusia 4 * Курорт Еленіте.",
     },
     {
       image: Destination6,
-      title: "London",
-      subTitle: "London, the capital of England and the United",
+      title: "Кіпр",
+      subTitle: "Кіпр, орієнтований на активну молодь, пропонує безліч розваг, як денних, так і нічних. Тим, хто не хоче упускати плюси сонячних днів, варто відправитися на пляж і спробувати себе у дайвінгу, серфінгу або орендувати яхту і відправитися у відкрите море. Так, на курорті Айя-Напа варто відвідати клуби Castle і Soho, Диско бар Senior Frog, в яких вирує життя, грають кращі діджеї Європи, змішують дивовижні коктейлі і танцюють до повної знемоги. Можна відправитися в бар-ресторан Kahlua, щоб відновити сили, і пограти в місцевому казино.",
       cost: "38,800",
       duration: "Approx 3 night 2 day trip",
     },
@@ -124,12 +114,13 @@ const Section = styled.section`
     gap: 3rem;
     padding: 0 3rem;
     .destination {
-      padding: 1rem;
+      //padding: 1rem;
+      padding: 20px;
       display: flex;
       flex-direction: column;
       gap: 0.5rem;
       //background-color: #8338ec14;
-      background-color: #3893ec;
+      background-color: #e4f0fb;
       border-radius: 1rem;
       transition: 0.3s ease-in-out;
       &:hover {
