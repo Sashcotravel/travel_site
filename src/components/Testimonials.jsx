cce56cc266be203ccd4915c70d43b5427466abda
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-09-20 21:54:18 +0300

    update photo

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -22,7 +22,7 @@ export default function Testimonials() {
               <li style={{listStyleType: 'disclosure-closed'}}>тури, візи, страхування</li>
               <li style={{listStyleType: 'disclosure-closed'}}>більше 5 років на туристичному ринку</li>
               <li style={{listStyleType: 'disclosure-closed'}}>відвідала більше 500 готелей світу</li>
-              <li style={{listStyleType: 'disclosure-closed'}}>більше 3000 тисяч задоволених туристів</li>
+              <li style={{listStyleType: 'disclosure-closed'}}>більше 3000 задоволених туристів</li>
             </ul>
           </div>
         </div>
